a8b6f48de3b6afe28931d450952cba6625773e08
nk3203-code <nk3203@srmist.edu.in>
2026-03-10 09:49:15 +0530

    Inital Project set up

---
+++ src/Arrayinitialisation.java
@@ -0,0 +1,21 @@
+public class Arrayinitialisation {
+    public static void main(String[] args) {
+        System.out.println("OOPS Banner App - UC5");
+
+        // Inline array initialization with String.join()
+        String[] bannerLines = {
+            String.join("", "   ***      ", "     ***     ", "  ******      ", "   ******   "),
+            String.join("", " **   **    ", "   **   **   ", "  **    **    ", " **         "),
+            String.join("", "**     **   ", "  **     **  ", "  **     **   ", " **         "),
+            String.join("", "**     **   ", "  **     **  ", "  **    **    ", "   **       "),
+            String.join("", "**     **   ", "  **     **  ", "  ******      ", "     ***    "),
+            String.join("", "**     **   ", "  **     **  ", "  **          ", "        **  "),
+            String.join("", "**     **   ", "  **     **  ", "  **          ", "         ** "),
+            String.join("", " **   **    ", "   **   **   ", "  **          ", "        **  "),
+            String.join("", "   ***      ", "     ***     ", "  **          ", "   ******   ")
+        };
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
